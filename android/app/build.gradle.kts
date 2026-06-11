ae42e0bd4269ed7e2b91e9c1e526f02de413c299
Nivea Nyctra <rizki.adsu@gmail.com>
2026-06-11 12:05:56 +0700

    Apply Flutter migrator changes and register sqflite
    Update Android build scripts to use the new Kotlin DSL placement and JVM target: remove the kotlin-android plugin and kotlinOptions block from app/build.gradle.kts and add a kotlin { compilerOptions { jvmTarget = JvmTarget.JVM_17 } } block. Add Flutter migrator flags to android/gradle.properties (android.builtInKotlin=false, android.newDsl=false). Also register the sqflite_darwin plugin on macOS by importing sqflite_darwin and calling SqflitePlugin.register in GeneratedPluginRegistrant.swift.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -13,10 +12,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -38,6 +33,13 @@ android {
         }
     }
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
 
 flutter {
     source = "../.."
